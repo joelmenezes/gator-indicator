aeeb160da9accbe04043ea53c3b25a10a8dfdf38
Joel Menezes <joelmenezes@healthsteps.io>
2018-06-01 11:40:34 -0700

    Changed dialog box when a bus stop is clicked

--- gator-indicator-frontend/src/test2.js
+++ gator-indicator-frontend/src/test2.js
@@ -1,50 +1,88 @@
 import React from "react";
 import { Marker, withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
+import axios from 'axios';
+const { InfoBox } = require("react-google-maps/lib/components/addons/InfoBox");
 
 const MyMapComponent = withScriptjs(withGoogleMap(props => {
-    // let ref = {};
-    const { onMapMounted, ...otherProps } = props;
-    return <GoogleMap {...otherProps} ref={c => {
-        onMapMounted && onMapMounted(c)
-    }}>{props.children}</GoogleMap>
+	const { onMapMounted, ...otherProps } = props;
+	return <GoogleMap {...otherProps} ref={c => {
+		onMapMounted && onMapMounted(c)
+	}}>{props.children}</GoogleMap>
 }))
 
+const busIcon = {
+	url: 'https://www.realestate-valdeurope.com/wp-content/themes/red/images/marker/icon-bus.png'
+}
 export default class MapComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { map: null };
-    }
-    mapLoaded(map) {
-        //console.log('mapLoaded: ' + JSON.stringify(map.getCenter()))
-        if (this.state.map !== null)
-            return
-        this.setState({ map: map })
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			map: null,
+			stops: []
+		};
+	}
 
-    mapMoved() {
-        console.log('mapMoved: ' + JSON.stringify(this.state.map.getCenter()))
-    }
+	componentDidMount() {
+		axios.get('http://localhost:5000/get-all-stops')
+			.then(res => {
+				let stops = res.data.map(el => {
+					var stop = Object.assign({}, el);
+					stop.showInfo = false
+					return stop;
+				});
+				this.setState({ stops })
+			})
+	}
+	mapLoaded(map) {
+		if (this.state.map !== null) return;
+		this.setState({ map: map })
+	}
 
-    zoomChanged() {
-        console.log(this.state.map.getZoom())
-    }
+	markerClicked = (stop_id) => {
+		let stops = this.state.stops;
+		let indexOfStop = stops.findIndex(stop => stop.stop_id === stop_id);
 
-    render() {
-        
-
-        return (
-            <MyMapComponent
-                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDpsOhFlgGnO7lyW5Nn-obvTWrTVxfQIMI&libraries=geometry,drawing,places"
-                loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{ height: `462px` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
-                defaultZoom={3}
-                defaultCenter={{ lat: 25.0391667, lng: 121.525 }}
-            >
-                <Marker
-                    position={{ lat: 25.039160, lng: 121.5251 }}
-                />
-            </MyMapComponent>
-        )
-    }
+		stops[indexOfStop].showInfo = !stops[indexOfStop].showInfo;
+		this.setState({ stops });
+	}
+	render() {
+		let stops = this.state.stops;
+		return (
+			<MyMapComponent
+				googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDpsOhFlgGnO7lyW5Nn-obvTWrTVxfQIMI&libraries=geometry,drawing,places"
+				loadingElement={<div style={{ height: `100%` }} />}
+				containerElement={<div style={{ height: `462px` }} />}
+				mapElement={<div style={{ height: `100%` }} />} defaultZoom={14}
+				defaultCenter={{ lat: 29.648403, lng: -82.360650 }}
+			>
+				{stops.map((stop, index) => {
+					return <Marker
+						key={index}
+						position={{ lat: parseFloat(stop.latitude), lng: parseFloat(stop.longitude) }}
+						icon={busIcon}
+						onClick={() => this.markerClicked(stop.stop_id)}
+						clickable={true}
+					>
+						{stop.showInfo &&
+							<InfoBox
+								options={{ closeBoxURL: ``, enableEventPropagation: true }}
+							>
+								<div style={{
+									backgroundColor: `white`, padding: `12px`, top: 9,
+									left: 15,
+									width: 203
+								}}>
+									<div style={{ fontSize: `12px`, fontColor: `#08233B` }}>
+										{stop.name}
+										<br />
+										{stop.routes}
+									</div>
+								</div>
+							</InfoBox>
+						}
+					</Marker>
+				})}
+			</MyMapComponent>
+		)
+	}
 }
